71cc8a105026ed7ce415dac6a610d43999e8b180
ukaracivio <umit.karacivi@outlook.com>
2023-12-11 11:52:07 +0300

    Add project files.

---
+++ P01_MVCBasics/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P01_MVCBasics.Models;
+using System.Diagnostics;
+
+namespace P01_MVCBasics.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(); // Yani bir View'a git...'Index.cshtml
+        }
+
+        public IActionResult Privacy()
+        {
+            return View(); // Privacy.cshtml...yönlendirecek
+        }
+
+        public IActionResult Contact()
+        {
+            return View(); // Privacy.cshtml...yönlendirecek
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ P02_Models/Models/StudentRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace P02_Models.Models
+{
+    public class StudentRepository : IStudentRepository
+    {
+
+
+        // DB de olmayan, InMemory türünde bir kayıtlar oluşturuyorum. Ve bunları bir Liste yapısı halinde tutuyorum.
+
+        public List<Student> DBTable()
+        {
+            return new List<Student>()
+            { 
+                new Student() {StudentId=1, Name="Ümit KARAÇİVİ", Email="umit.karacivi@outlook.com", Phone="05523346114",Gender="E"},
+                new Student() {StudentId=2, Name="Çekdar ÇAPAR", Email="cc@gmail.com", Phone="09998887766",Gender="E"},
+                new Student() {StudentId=3, Name="Nisa Öykü YURDAGÜL", Email="noy@gmail.com", Phone="01112223344",Gender="K"},
+                new Student() {StudentId=4, Name="Emine Ayça BAŞARAN", Email="eab@gmail.com", Phone="04443335566",Gender="K"}
+            };
+        }
+
+        public Student Get(int StudentID)
+        {
+            return DBTable().FirstOrDefault(e => e.StudentId == StudentID) ?? new Student();
+        }
+    }
+}
