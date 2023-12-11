46f5e4dc093cc18d4603d233d6767a3eef5f8278
ukaracivio <umit.karacivi@outlook.com>
2023-12-11 14:06:06 +0300

    Some changes

--- P03_Controllers/Controllers/StudentController.cs
+++ P03_Controllers/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace P03_Controllers.Controllers
 {
@@ -8,6 +9,19 @@ namespace P03_Controllers.Controllers
         public string GetAll()
         {
             return "--- Tüm öğrenciler ---";
+        }
+        public string GetOneStudent(string name)
+        {
+            return $"Kayıdını istemiş olduğunuz öğrenci : {name}";
+        }
+
+        public string Topla(int sayi1,int sayi2) 
+        {
+            int sonuc;
+
+            sonuc=sayi1+sayi2;
+            
+            return $"Toplam sonucu : {sonuc.ToString()}";
         }
 
         public IActionResult Index()
