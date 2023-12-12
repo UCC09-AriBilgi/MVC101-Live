86d0bf4cc1c8171a3355c04c9481b7e47c661224
ukaracivio <umit.karacivi@outlook.com>
2023-12-12 09:47:24 +0300

    Some changes

--- P04_Views/Controllers/HomeController.cs
+++ P04_Views/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace P04_Views.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ P04_Views/Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P04_Views.Models;
+
+namespace P04_Views.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            Student student = new Student()
+            {
+                StudentId = 200,
+                Name = "Ümit K",
+                Phone = "12345678901",
+                Email = "xx@yy.com",
+                Gender = "E"
+            };
+
+            ViewData["StudentInfo"] = student;
+
+
+
+
+
+            return View(ViewData["StudentInfo"]);
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
